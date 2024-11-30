444c5d410064ebcecfaa952fc46b288d3e594135
Abdelhalem Kassara <abdelhalem@gmail.com>
2024-11-30 17:57:18 -0500

    initial working version with some bugs and missing features

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//runs whenever the user saves a file (also runs when the file is autosaved)
 	const disposable4 = vscode.workspace.onDidSaveTextDocument(async (e) => {
+
 		// out.appendLine(e.fileName)s;//gets the file path and name
 		// out.appendLine(e.getText());//gets the content of the file
 
@@ -51,7 +52,7 @@ export function activate(context: vscode.ExtensionContext) {
 			let path: vscode.Uri = URI.parse("file://" + e.fileName + "-example");
 			await vscode.workspace.fs.writeFile(path, new TextEncoder().encode(removeValFromEnv(fileContents)));//this writes regardless if there is a file there or not
 		} catch(e: any) {
-			out.append(e.toString());
+			out.appendLine(e.toString());
 		}
 	});
 
@@ -67,6 +68,12 @@ export function activate(context: vscode.ExtensionContext) {
 		endOfSecret // use this when you have reached the end of the secret (it's to check if there is a comment there)
 	}
 
+	//note that the format of .env isn't 100% the same on all programming languages but this should probably work for most
+
+	//double check all the cases where skipChar() function is used that it is skipping by the correct amount
+	//possible bug: the comments inside multiline values are being put into the .env-example
+		//remove the whitespace for multiline .env variables
+	//bug: it should be possible to have multiline strings with single and double quotes (not the ''' """, just ' and ")
 	function removeValFromEnv(fileContents: string): string {
 		let contByLine: string[] = fileContents.split('\n');
 
@@ -75,14 +82,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 		let cleanContByLine: string[] = new Array(contByLine.length).fill("");
 
+		let line = 0;
+		let char = 0;
 
-		for(let line = 0; line < contByLine.length; line++) {
-			for(let char = 0; char < contByLine[line].length; char++) {
+		for(; line < contByLine.length; line++) {
+			char = 0;
+			for(; char < contByLine[line].length; char++) {
 				if(contByLine[line][char] === '#') {
-					cleanContByLine[line] += contByLine[line].slice(char, contByLine[line].length); //double check this is slicing properly
+					cleanContByLine[line] += contByLine[line].slice(char, contByLine[line].length);
 
 					if(strType === stringTypes.single || strType === stringTypes.double) {
-						throw new Error(`Invalid env file, the string on line number ${line} does have a closing quote.`);
+						throw new Error(`Invalid env file, the string on line number ${line + 1} does have a closing quote.`);
 					}
 					break;
 				}
@@ -90,59 +100,73 @@ export function activate(context: vscode.ExtensionContext) {
 				//probably should break up each case into it's own function
 				switch(strType) {
 					case stringTypes.none:
-						if(contByLine[line][char] === '=') {
-							if(char === 0) {//probably don't need this with the regex underneath
-								throw new Error(`Invalid env file, line number ${line} shouldn't start with an equal sign.`);
-							}
-		
-							
+						if(contByLine[line][char] === '=') {						
+							//adds the variable name	
 							if(!pattern.test(contByLine[line].slice(0, char))) {
-								throw new Error(`Invalid env file, the variable name in line number ${line} is invalid.`);
+								throw new Error(`Invalid env file, the variable name in line number ${line + 1} is invalid.`);
 							} else {
 								cleanContByLine[line] += contByLine[line].slice(0, char+1) + ' ';
 							}
-		
+							
+							//determines the type of value
 							if(contByLine[line].length - char+1 >= 3) {
 	
 								if(contByLine[line][char+1] === '"' && contByLine[line][char+2] === '"' && contByLine[line][char+3] ===  '"') {
 									strType = stringTypes.multilineDouble;
+									skipChar(3);
 								} else if(contByLine[line][char+1] === "'" && contByLine[line][char+2] === "'" && contByLine[line][char+3] === "'") {
 									strType = stringTypes.multilineSingle;
+									skipChar(3);
 								}
 							} else if(contByLine[line].length - char+1 >= 1) {
 								
 								if(contByLine[line][char+1] === '"') {
 									strType = stringTypes.double;
+									skipChar(1);
 								} else if(contByLine[line][char+1] === "'") {
 									strType = stringTypes.single;
+									skipChar(1);
 								}
 							} else {
 								strType = stringTypes.notQuote;
+								skipChar(1);
 							}
 						}
 						break;
 
 					case stringTypes.multilineDouble:
+						if(contByLine[line].length - char+1 >= 2 && contByLine[line][char-1] !== '\\' && contByLine[line][char] === '"' && contByLine[line][char+1] === '"' && contByLine[line][char+2] ===  '"') {
+							strType = stringTypes.endOfSecret;
+							skipChar(2);
+						} else if(line === contByLine.length-1 && char === contByLine[line].length-3) {//reached the end of the file
+							throw new Error(`Invalid env file, there is a missing endquote for a multiline quote.`);
+						}
 						break;
 					
 					case stringTypes.multilineSingle:
+						if(contByLine[line].length - char+1 >= 2 && contByLine[line][char-1] !== '\\' && contByLine[line][char] === "'" && contByLine[line][char+1] === "'" && contByLine[line][char+2] ===  "'") {
+							strType = stringTypes.endOfSecret;
+							skipChar(2);
+						} else if(line === contByLine.length-1 && char === contByLine[line].length-3) {//reached the end of the file
+							throw new Error(`Invalid env file, there is a missing endquote for a multiline quote.`);
+						}
 						break;
 
 					case stringTypes.double:
-						//check if the previous character is '\' (for escaping the quote) (no need to check the previous character on the first iteration as it's guaranteed to be an equal sign)
-						if(contByLine[line][char] === '"' && contByLine[line][char-1] !== '\\') {
+						//checks if the previous character is '\' (for escaping the quote) (doesn't matter if we check the previous character on the first iteration as it's guaranteed to be the opening double quote)
+						if(contByLine[line][char] === "'" && contByLine[line][char-1] !== '\\') {
 							strType = stringTypes.endOfSecret;
 						} else if(char === contByLine[line].length-1) {
-							throw new Error(`Invalid env file, there is a missing double quote on line ${line}.`);
+							throw new Error(`Invalid env file, there is a missing double quote on line ${line + 1}.`);
 						}
 						break;
 
 					case stringTypes.single:
-						//check if the previous character is '\' (for escaping the quote) (no need to check the previous character on the first iteration as it's guaranteed to be an equal sign)
+						//checks if the previous character is '\' (for escaping the quote) (doesn't matter if we check the previous character on the first iteration as it's guaranteed to be the opening double quote)
 						if(contByLine[line][char] === "'" && contByLine[line][char] !== '\\') {
 							strType = stringTypes.endOfSecret;
 						} else if(char === contByLine[line].length-1) {
-							throw new Error(`Invalid env file, there is a missing double quote on line ${line}.`);
+							throw new Error(`Invalid env file, there is a missing double quote on line ${line + 1}.`);
 						}
 						break;
 					
@@ -155,12 +179,10 @@ export function activate(context: vscode.ExtensionContext) {
 					case stringTypes.endOfSecret:
 						//there should only be white space after the end of the secret (the code at the start of the for loop will catch the comment if it's there)					
 						if(contByLine[line][char] !== ' ') {
-							throw new Error(`Invalid env file, there should only be white space after the end of the secret on ${line}.`);
+							throw new Error(`Invalid env file, there should only be white space after the end of the secret on line ${line + 1}.`);
 						}
 						break;
-				}
-
-				
+				}				
 			}
 			if(strType === stringTypes.endOfSecret) {
 				strType = stringTypes.none;
@@ -168,8 +190,18 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 
 		return cleanContByLine.reduce((acc, cur) => acc + '\n' + cur);
+
+
+		function skipChar(qty: number) { //doesn't check if it reached the end of the file, the for loop will take care of that
+			char += qty;
+
+			line += Math.floor(char / contByLine[line].length);
+			char %= contByLine[line].length;
+		}
 	}
 }
+
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
